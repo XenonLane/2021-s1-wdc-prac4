51c3b0163ef80217c04e440c775db0b2859b4af2
Xenon Lane <xenonlane25@gmail.com>
2021-03-31 10:35:05 +0000

    task 4.2

---
+++ public/javascripts/title_change.js
@@ -0,0 +1,15 @@
+function title_change()
+{
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          // Typical action to be performed when the document is ready:
+          document.getElementById("title").innerHTML = xhttp.responseText;
+          document.getElementById("title").style.color = xhttp.responseText;
+        }
+    };
+
+    xhttp.open("GET", "/color.txt", true);
+    xhttp.send();
+}
